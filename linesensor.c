11a51b38e63dcb8eb353fa862a564a241d2f7499
adnusan <ad.nusan@gmail.com>
2022-11-11 14:48:35 -0800

    fixed threads

--- linesensor.c
+++ linesensor.c
@@ -2,6 +2,8 @@
 #include <pigpio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
+
 
 
 
@@ -15,66 +17,73 @@
 
 void *line_sensor();
 
-int left;
+struct LineSensorData
+{
+	char * sensor_name;
+	int gpioPin;
+};
+
+
+
+
 int main(){
 	//name of sensor for more readability
 	char l_sensor[] = "LEFT SENSOR";
 	char r_sensor[] = "RIGHT SENSOR";
+
+	//line sensor data
+	struct LineSensorData *LeftLine = (struct LineSensorData *)malloc(sizeof(struct LineSensorData));
+    struct LineSensorData *RightLine = (struct LineSensorData *)malloc(sizeof(struct LineSensorData));
+
+	//setting sensor data name and var for easy readability
+	LeftLine->sensor_name = l_sensor;
+	LeftLine->gpioPin = LEFT_LINE_SENSOR;
+	RightLine->sensor_name = r_sensor;
+	RightLine->gpioPin = RIGHT_LINE_SENSOR;
+
+	//initialize pigpio
 	if(gpioInitialise() < 0){
 		return 1;
 	}
 	
+	//setting gpio mode to input
 	gpioSetMode(LEFT_LINE_SENSOR, PI_INPUT);
 	gpioSetMode(RIGHT_LINE_SENSOR, PI_INPUT);
 	
 	//waiting for gpio to  calibrate
 	gpioDelay(5);
-	
-	//creating thread for line sensor and obstacle sensor
+
+
+	//threads for line sensor
 	pthread_t left_line_sensor_thread, right_line_sensor_thread;
-	if(pthread_create(&left_line_sensor_thread, NULL, line_sensor(LEFT_LINE_SENSOR,l_sensor), NULL)){
-		printf("error \n");
+	if(pthread_create(&left_line_sensor_thread, NULL, line_sensor, (void *)LeftLine) != 0){
+		printf("Error creating thread");
 	}
-//TODO: run two line sensor in thread and detect which way to turn
-
-	if(pthread_create(&right_line_sensor_thread, NULL, line_sensor(RIGHT_LINE_SENSOR,r_sensor), NULL)){
-		printf("error \n");
+	if(pthread_create(&right_line_sensor_thread, NULL, line_sensor, (void *)RightLine) != 0){
+		printf("Error creating thread");
 	}
 
 
-//	for (int i = 0; i < 10; ++i){
-//	while(1){
-	//	if(left == 0){
-	//		printf("WHITE: %d", left);
-	//	}
-	//	if(left == 1){
-	//		printf("BLACK: %d", left);
-	//	}
-	//	printf("\n");
-	//	sleep(1);
-		line_sensor(RIGHT_LINE_SENSOR, r_sensor);
-		line_sensor(LEFT_LINE_SENSOR, l_sensor);
-		printf("after left line");
-		
-//		sleep(1);
-//	}
+
+	
+	//waiting for thread to end
 	pthread_join(left_line_sensor_thread, NULL);
 	pthread_join(right_line_sensor_thread, NULL);
-    	printf("Thread ended.\n"); 
+    printf("Thread ended.\n"); 
 	gpioTerminate(); //terminate the gpio
-    	return 0;
+    return 0;
 }
 
 
-void *line_sensor(int sensor_gpio, const char *name){
-//	for(int i = 0; i < 10; i++){	
-while(1){
-//		left = gpioRead(sensor_gpio);
-	//	printf("Value: %d", temp);
-		if(gpioRead(sensor_gpio) == 1){
+void *line_sensor(void* input){
+
+	char  *name = ((struct sensorInfo*)input)->name;
+	int gpioPin = ((struct sensorInfo*)input)->sensorGpio;
+	while(1){
+		if(gpioRead(gpioPin) == 1){
 			printf("%s on BLACK!\n", name);
 		}
-		if(gpioRead(sensor_gpio) == 0) {
+		if(gpioRead(gpioPin) == 0) {
 			printf("%s on WHITE!\n", name);
 		}
 	sleep(1);
