bcb667d31fbae52d98cbdb76bb22a7fa7b452dae
adnusan <ad.nusan@gmail.com>
2022-11-12 00:35:29 -0800

    cleaned the code

--- linesensor.c
+++ linesensor.c
@@ -1,11 +1,10 @@
-/*
-* Program to run multiple line sensor BLACK thread
-* @author Nusan Rana
-*  sensor value 0 == white line, 1 == black line
+/*@author Nusan Rana
+*Program to run multiple line sensor on thread
+*Added logics to handle turns.
+*Left sensor must connect to gpio #14 and right gpio #18
+*Line Sensor value: 0 == white line, 1 == black line 
+*gcc -Wall -o linesensor linesensor.c -lpigpio -lpthread
 */
-
-
-
 #include <stdio.h>
 #include <pigpio.h>
 #include <unistd.h>
@@ -13,19 +12,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-
 //defining sensors gpio
 #define LEFT_LINE_SENSOR 14
 #define RIGHT_LINE_SENSOR 18
 #define BLACK 1
 #define WHITE 0
 
-//functiBLACK prototypes
-
-void *line_sensor();
-void *line_sensor_cBLACKtrol();
+//function prototypes
+void *line_sensor_control();
 
 struct LineSensorData
 {
@@ -70,31 +64,32 @@ int main(){
 
 	//creating threads, passing struct which has sensor name and sensor gpio pin #
 	if(pthread_create(&left_line_sensor_thread, NULL, line_sensor_control, (void *)LeftLine) != 0){
-		printf("Error creating thread");
+		printf("Error creating thread\n");
 	}
 	if(pthread_create(&right_line_sensor_thread, NULL, line_sensor_control, (void *)RightLine) != 0){
-		printf("Error creating thread");
+		printf("Error creating thread\n");
 	}
 
+
+	//checking the updated value of sensor
 	while(1){
 		//sensor value 0 == white line, 1 == black line
+		//if both sensor is on black line then go straight
 		if (LeftLine->updated_data == BLACK && RightLine->updated_data == BLACK){
 			printf("Both Line Sensor on Black line: R:%d  L:%d\n Go straight: %d\n", RightLine->updated_data, LeftLine->updated_data);
 		}
+
+		//if left sensor detect white then left side of car is out of lane, turn right to stay on black lane
 		if(LeftLine->updated_data == WHITE && RightLine->updated_data == BLACK){
 			printf("Left Line Sensor on White line: R:%d  L:%d\n TURN RIGHT!: %d\n", RightLine->updated_data, LeftLine->updated_data);
 		}
+		//if right sensor detect white then right side of car is out of lane, turn left to stay on black lane
 		if(LeftLine->updated_data == BLACK && RightLine->updated_data == WHITE){
 			printf("Right Line Sensor on White line: R:%d  L:%d\n TURN LEFT!: %d\n", RightLine->updated_data, LeftLine->updated_data);
 		}
-
-		///printf("Left Sensor: %d\n", LeftLine->updated_data);
-		//printf("Right Sensor: %d\n", RightLine->updated_data);
 		sleep(1);
 	}
 
-
-	
 	//waiting for thread to end
 	pthread_join(left_line_sensor_thread, NULL);
 	pthread_join(right_line_sensor_thread, NULL);
@@ -103,46 +98,17 @@ int main(){
     return 0;
 }
 
-//this functiBLACK take in a pointer to a struct LineSensorData
+//this function take in a pointer to a struct LineSensorData
 //and access the name and value of gpio from the struct
-//TODO: add a way to stop the loop
-void *line_sensor(void* input){
-
-	struct LineSensorData *sensor_data = (struct LineSensorData *)input;
-
-	char  *name = sensor_data->sensor_name;
-	int gpioPin = sensor_data->gpioPin;
-	while(1){
-		if(gpioRead(gpioPin) == 1){
-			printf("%s BLACK BLACK!\n", name);
-
-		}
-		if(gpioRead(gpioPin) == 0) {
-			printf("%s BLACK WHITE!\n", name);
-		}
-	sleep(1);
-	}
-}
-
-//TODO: check the value or line sensor and detect which way to turn
+//updates the value of updated_data from gpioRead(gpio)
+//could modify this function to get data from any sensor
 void *line_sensor_control(void* input){
 	struct LineSensorData *sensor_data = (struct LineSensorData *)input;
-	char  *name = sensor_data->sensor_name;
+	//char  *name = sensor_data->sensor_name;
 	int gpioPin = sensor_data->gpioPin;
 	while(1){
 		sensor_data->updated_data = gpioRead(gpioPin);
-		// if(gpioPin == 1){
-		// 	printf("%s BLACK BLACK!\n", name);
-		// 	sensor_data->updated_data=1;
-		// }
-		// if(gpioPin == 0) {
-		// 	printf("%s BLACK WHITE!\n", name);
-		// }
-		// RIGHT_LINE_SENSOR = 0;
-
-	sleep(2);
+	sleep(1);
 	printf("\n");
 	}
-
 }
-
